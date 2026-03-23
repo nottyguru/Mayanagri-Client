6fb5263e833f3935c3ac954d1ccb1c5fb1c8f268
nottyguru <unemaysh@gmail.com>
2026-03-23 16:36:34 +0530

    ggg

--- generate_manifest.py
+++ generate_manifest.py
@@ -2,13 +2,11 @@ import os
 import hashlib
 import json
 
-# The root URL for your raw GitHub files
 BASE_URL = "https://raw.githubusercontent.com/nottyguru/Mayanagri-Client/main/files/"
 FILES_DIR = "files"
 
 
 def calculate_sha1(file_path):
-    """Generates the cryptographic hash for file integrity."""
     sha1 = hashlib.sha1()
     with open(file_path, "rb") as f:
         while chunk := f.read(8192):
@@ -19,35 +17,28 @@ def calculate_sha1(file_path):
 def build_manifest():
     file_entries = []
 
-    # Walk through the 'files' directory
     for root, _, files in os.walk(FILES_DIR):
         for file in files:
             local_path = os.path.join(root, file)
-
-            # Get the path relative to the 'files' folder (e.g., 'mods/sodium.jar')
             relative_path = os.path.relpath(local_path, FILES_DIR).replace("\\", "/")
-
-            # Build the raw download URL
             download_url = BASE_URL + relative_path
-
-            # Calculate the hash
             file_hash = calculate_sha1(local_path)
 
             file_entries.append(
                 {"path": relative_path, "url": download_url, "sha1": file_hash}
             )
 
-    # The static server settings
+    # ADDED: java_version is now dynamically set here
     manifest_data = {
         "minecraft_version": "1.20.4",
         "fabric_version": "0.15.7",
+        "java_version": 17,
         "server_ip": "play.mayanagri.net",
         "server_port": 25565,
         "jvm_flags": ["-XX:+UseG1GC", "-XX:MaxGCPauseMillis=50", "-Xmn128M"],
         "files": file_entries,
     }
 
-    # Write it to the JSON file
     with open("modpack_manifest.json", "w") as f:
         json.dump(manifest_data, f, indent=4)
 
